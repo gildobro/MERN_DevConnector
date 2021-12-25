6ab66df56014b5a1415e46c2b47de6840a14e258
gildobro <60789242+gildobro@users.noreply.github.com>
2021-12-25 15:37:54 -0500

    Created backend functionality for user registration
    -user now properly adds to the database
    
    -errors are caught and handled

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,72 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+//this was depracated: 'express-validator/check' --- if issues arise, replace above with this.
+
+// Get user model
+const User = require('../../models/User');
 
 // @route  POST api/users
 // @desc   Register user
 // @access Public
-router.post('/', [
-    check('name', 'Name is required')
-    .not()
-    .isEmpty(),
-    check('email', 'Please include a valid email')
-    .isEmail(),
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-        'password', 
-        'Please enter a password with 6 or more characters'
-        ).isLength({min: 6})
-],
-(req,res) => {
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
-});
 
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
