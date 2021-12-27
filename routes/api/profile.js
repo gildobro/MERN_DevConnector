93f067f3b6ba3e095613a6372099af179d98112b
gildobro <60789242+gildobro@users.noreply.github.com>
2021-12-26 23:32:49 -0500

    Added route to profile.js to delete user profile work experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,3 +1,5 @@
+//@Todo = Update user work experience & user education (reference other updates)
+
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -207,5 +209,28 @@ router.put(
     }
   }
 );
+
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   DELETe experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
