b0460187d4a8afff43487cccc60d55889d74afee
gildobro <60789242+gildobro@users.noreply.github.com>
2022-01-04 17:37:57 -0500

    Able to Register and Authenticate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,28 +8,39 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Landing />} />
-        </Routes>
-        <section className='container'>
-          <Alert />
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Routes>
-            <Route exact path='/register' element={<Register />} />
-            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/' element={<Landing />} />
           </Routes>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route exact path='/register' element={<Register />} />
+              <Route exact path='/login' element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
 
