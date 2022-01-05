79e774b7a34645f887e1a32c6bdc187fda7fdfaf
gildobro <60789242+gildobro@users.noreply.github.com>
2022-01-05 14:42:30 -0500

    Created Protected Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -34,6 +36,10 @@ const App = () => {
             <Routes>
               <Route exact path='/register' element={<Register />} />
               <Route exact path='/login' element={<Login />} />
+              <Route
+                path='dashboard'
+                element={<PrivateRoute component={Dashboard} />}
+              />
             </Routes>
           </section>
         </Fragment>
