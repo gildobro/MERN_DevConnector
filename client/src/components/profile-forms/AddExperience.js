5adff06f36c761bfb0958d4aedcc65daa91ba0db
gildobro <60789242+gildobro@users.noreply.github.com>
2022-01-06 04:08:46 -0500

    Apprehended some warnings

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profile';
