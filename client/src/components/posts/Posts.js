0bf767a341d72dfd60a398d66f9dce8fa7cc3742
gildobro <60789242+gildobro@users.noreply.github.com>
2022-01-07 15:37:40 -0500

    Added Post Item Component
    -Populated items with data
       -likes
        -text
        -discussion, etc

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 import { propertiesParser } from 'config/parser';
 
@@ -10,7 +11,22 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large test-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* Post Form**/}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
