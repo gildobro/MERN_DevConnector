46680e514103742e0ed058d6660480c3d24946c1
gildobro <60789242+gildobro@users.noreply.github.com>
2022-01-07 17:22:35 -0500

    Comment Display and Delete (UI of Application is now fully functional)

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
 import { Link, useParams } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   const { id } = useParams();
@@ -22,6 +23,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
